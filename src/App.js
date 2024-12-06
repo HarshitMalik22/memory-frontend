c87ea695257ce92a6a106094b8a4d74b9e78c7d9
HarshitMalik22 <130777707+HarshitMalik22@users.noreply.github.com>
2024-12-06 18:05:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';  // Using Switch and Route from v5
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Home from './components/pages/Home';
 import Signin from './components/pages/Signin';
 import Register from './components/pages/Register';
 import Game from './components/pages/Game';
-import PrivateRoute from './components/routing/PrivateRoute';
+import PrivateRoute from './components/routing/privateRoute';
 
 import HistoryState from './context/history/HistoryState';
 import AuthState from './context/auth/AuthState';
 import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
-// If a token is found in localStorage, set it globally
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
 const App = () => {
   return (
-    // Auth and History contexts
+    // auth and history contexts
     <AuthState>
       <HistoryState>
-        {/* Router for navigation */}
+        {/* router for navigation */}
         <Router>
           <div className='App'>
-            {/* Navbar component */}
+            {/* navbar component */}
             <Navbar />
             <div className='Routes'>
-              {/* Private/public routes for the app */}
+              {/* private/public routes for app */}
               <Switch>
-                <PrivateRoute exact path="/" component={Home} />
-                <Route exact path="/signin" component={Signin} />
-                <Route exact path="/register" component={Register} />
-                <PrivateRoute exact path="/game" component={Game} />
+                <PrivateRoute exact path='/' component={Home} />
+                <Route exact path='/signin' component={Signin} />
+                <Route exact path='/register' component={Register} />
+                <PrivateRoute exact path='/game' component={Game} />
               </Switch>
             </div>
           </div>

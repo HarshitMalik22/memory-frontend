ad93c365c71122ec9ff6d3628aef7476f2297113
HarshitMalik22 <130777707+HarshitMalik22@users.noreply.github.com>
2024-12-06 17:13:14 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,2 @@
 export const BASE_URL = 'https://memory-backend-production-1b1e.up.railway.app';
 
-const res = await fetch(`${BASE_URL}/api/auth`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ email, password }),
-});
